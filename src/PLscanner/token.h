5702e5521ffacb0f411ce6cb7917832c6d7e3966
Eric (Eric) DenHaan <dene3740@36mystique.cs.uleth.ca>
2019-01-16 16:34:21 -0700

    Update to project structure

---
+++ src/PLscanner/token.h
@@ -0,0 +1,34 @@
+//************************************************************************************
+// CPSC 4600
+// PL Language Scanner
+// Token Interface File
+// Written By: Eric Den Haan and Blake Nelson
+//************************************************************************************
+
+#ifndef TOKEN_H
+#define TOKEN_H
+
+#include "./symbol.h"
+#include <string>
+
+using namespace std;
+
+class Token
+{
+  // Constructors
+  Token();
+  Token(const Symbol&);
+  Token(const Symbol&, const string&);
+
+  // Public Methods
+  void getSname();
+  void setSname();
+  void getSval();
+  void setSval();
+
+  // Data Members
+  Symbol sname;
+  string sval;
+};
+
+#endif
---
+++ src/PLsymboltable/symbolTable.h
@@ -0,0 +1,23 @@
+//************************************************************************************
+// CPSC 4600
+// PL Language Compiler
+// Symbol Table Interface File
+// Written By: Eric Den Haan and Blake Nelson
+//************************************************************************************
+
+#ifndef SYMBOLTABLE_H
+#define SYMBOLTABLE_H
+
+using namespace std;
+
+class SymbolTable
+{
+  // Constructors
+
+  // Public Methods
+
+  // Data Members
+
+};
+
+#endif
