32e56594f2b651d6235571f97188ac89e630d674
Blakely (Blake) Nelson <nelb4600@36mystique.cs.uleth.ca>
2019-01-23 16:31:09 -0700

    Fleshed out some of Administration

--- src/administration.cc
+++ src/administration.cc
@@ -12,6 +12,9 @@ Administration::Administration(ifstream &inputFile, ofstream &outputFile, Scanne
 {
   scanner = &s;
   outputFilePtr = &outputFile;
+  //open the files
+  inputFile.open("./data.txt");
+  outputFile.open("./output.txt");
   currentLine = 0;
   errorCount = 0;
 }
@@ -21,4 +24,36 @@ Administration::~Administration()
 {
   scanner = nullptr;
   outputFilePtr = nullptr;
+}
+
+void Administration::newLine()
+{
+  //encounter a line error
+  if(correctLine == false)
+  {
+    //report error
+    //error("meaningful error message");
+    errorCount++;
+    if(errorCount == MAX_ERRORS)
+    {
+      cout << "maximum error count exceeded" << endl;
+      exit(0);
+    }
+    currentLine++;
+  }
+  correctLine = true;
+}
+
+void Administration::error(const string &s)
+{
+  cout << currentLine << " " << s << endl;
+}
+
+int Administration::scan()
+{
+  int indexOfToken = -1;
+  string line;
+  //Giving error on inputFile not being declared, but there is no ifstream data member
+  //getline(inputFile, line, '\n');
+  return indexOfToken;
 }
